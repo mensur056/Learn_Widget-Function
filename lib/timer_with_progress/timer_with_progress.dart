179f0706fe4f70d95b7b2ea718bcddd2b5dfdbc7
mensur056 <81297953+mensur056@users.noreply.github.com>
2022-10-28 00:30:33 +0400

    test

---
+++ lib/timer_with_progress/timer_with_progress.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class TimerView extends StatefulWidget {
+  const TimerView({Key? key}) : super(key: key);
+
+  @override
+  State<TimerView> createState() => _TimerViewState();
+}
+
+class _TimerViewState extends State<TimerView> {
+  Timer? _timer;
+  int _start = 20;
+  double value = 1;
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  void startTimer() {
+    const oneSec = Duration(seconds: 1);
+    _timer = Timer.periodic(
+      oneSec,
+      (Timer timer) {
+        if (_start == 0) {
+          setState(() {
+            timer.cancel();
+          });
+        } else {
+          setState(() {
+            _start--;
+            value -= 0.05;
+          });
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Timer test")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          LinearProgressIndicator(
+            value: value,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text("$_start")
+        ],
+      ),
+    );
+  }
+}
