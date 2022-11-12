1beb8b070786b2efcd4d0ccac8a35d34b80a1d5e
mensur056 <81297953+mensur056@users.noreply.github.com>
2022-11-12 11:09:56 +0400

    test

---
+++ lib/scrollable_sheet/scrollable_sheet.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ScrollableSheetUse extends StatefulWidget {
+  const ScrollableSheetUse({Key? key}) : super(key: key);
+
+  @override
+  State<ScrollableSheetUse> createState() => _ScrollableSheetUseState();
+}
+
+class _ScrollableSheetUseState extends State<ScrollableSheetUse> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SizedBox(
+        child: DraggableScrollableSheet(
+          initialChildSize: 0.4,
+          minChildSize: 0.2,
+          maxChildSize: 0.6,
+          builder: (BuildContext context, ScrollController scrollController) {
+            return Container(
+              decoration: BoxDecoration(
+                  color: Colors.blue[100],
+                  borderRadius: const BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+              child: ListView.builder(
+                controller: scrollController,
+                itemCount: 25,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(title: Text('Item $index'));
+                },
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
